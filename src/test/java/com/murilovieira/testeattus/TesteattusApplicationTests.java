c54c08769dc545681445f763a560b843119f4270
Murilo Vieira <murilo12super@gmail.com>
2024-04-05 09:11:03 -0300

    Inicio do projeto

---
+++ src/test/java/com/murilovieira/testeattus/TesteattusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.murilovieira.testeattus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteattusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
